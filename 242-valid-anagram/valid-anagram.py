5ae53cc8e84341155078d26bbe68566f9e98f353
Abdul Raziq <161387601+ARB726@users.noreply.github.com>
2026-09-08 18:37:30 -0700

    Time: 8 ms (82.89%) | Memory: 19.3 MB (94.73%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hashMap1 , hashMap2 = {} , {}
-        for char in s:
-            hashMap1[char] = hashMap1.get(char , 0) + 1
-        
-        for char in t:
-            hashMap2[char] = hashMap2.get(char , 0) + 1
-
-        return hashMap1 == hashMap2
+        return Counter(s) == Counter(t)
