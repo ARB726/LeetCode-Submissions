1a945804fadbfd9ab08e783edcf5a8651f18cf43
Abdul Raziq <161387601+ARB726@users.noreply.github.com>
2026-09-22 14:56:49 -0700

    Time: 603 ms (69.42%) | Memory: 22.3 MB (55.48%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,35 +1,49 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        nums.sort()
         result = []
-
+        nums.sort()
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            
+            if i > 0 and nums[i] == nums[i-1]: continue
             j = i + 1
-
             k = len(nums)-1
-            
+
             while j < k:
 
-                total = nums[i] + nums[j] + nums[k]
+                totalSum = nums[i] + nums[j] + nums[k]    
 
-                if total > 0:
+                if totalSum > 0:
                     k -=1
-
-                elif total < 0:
+                
+                elif totalSum < 0:
                     j +=1
 
                 else:
-                    result.append([nums[j],nums[k],nums[i]])
-
-                    while j < k and  nums[k] == nums[k-1]:
-                        k -=1
+                    result.append([nums[i],nums[j],nums[k]])
 
                     while j < k and nums[j] == nums[j+1]:
                         j +=1
+                    
+                    while j < k and nums[k] == nums[k-1]:
+                        k -=1
+                    
                     j+=1
                     k-=1
+
         return result
 
+
+
+    
+
+"""
+- create a empty list
+- sort array
+- use a for loop to iterate until next one is not same and index is greater than 0
+    - create a second pointer and third pointer
+    - use a while loop to check until 2nd and 3rd pointers don't meet
+    - if sum is greater than 0 decrease last pointer
+    - if sum is less than then increase second pointer
+    - else create a list and add to the original list
+    - after that shift second and third pointer as well
+
+"""
